c80b3ffe5b4ba776ff38f50b4b0e3630ba95a8f5
DavidChoi76 <choiyd1115@gmail.comn>
2024-10-21 02:17:26 +0900

    update

--- CCdownscaling/__init__.py
+++ CCdownscaling/__init__.py
@@ -2,5 +2,5 @@ from .correction_downscale_methods import *
 from .distribution_tests import *
 from .error_metrics import *
 from .tf_som import *
-from .som_downscale import *
+#from .som_downscale import *
 from .utilities import *
